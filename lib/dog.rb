b769ba4ad43fd2bd3e034a0dae01586fb0c8797e
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-12-07 22:39:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,36 @@
-class Dog
 
+
+require "pry"
+
+class Dog
+  attr_accessor :name 
+  @@all = [] 
+  
+def initialize(name)
+    @name = name
+    binding.pry
+   self.save
+  
+end
+
+def self.all
+    @@all
+end
+
+def self.print_all
+    @@all.each do |dog|
+    puts dog.name
+  end
+end
+ 
+ def self.clear_all
+     @@all.clear
+ end
+ 
+ def save
+   @@all << self
+ end
+ 
+  Dog.all
+  
 end
